1fd266df7f8c4a13cd0ed5dada09fe3ba95f4a5d
Alejandro Campillo <alucardcampillo@gmail.com>
2021-02-23 16:01:19 -0500

    docstrings updated

--- models/user.py
+++ models/user.py
@@ -22,11 +22,30 @@ class user_to_register(applicant_user):
     def password_match(cls, password_to_verify, values, **kwargs):
         """ Validate that the value taken by password and verify_password match
 
-        Keyword arguments:
-        cls -- takes user_to_register class as an argument
-        password_to_verify -- is the value in the verify_password field 
-        values -- is the value in the password field, by default the decorator takes this name as a parameter
-        **kwargs --  if provided, this will include the arguments above not explicitly listed in the signature, this is necessary for the function takes the key value pairs defined in the class         
+        Parameters
+        ----------
+        cls
+            takes user_to_register class as an argument
+        password_to_verify: str 
+            is the value in the verify_password field 
+        values: str
+            is the value in the password field, by default the decorator takes 
+            this name as a parameter
+        **kwargs:  
+            if provided, this will include the arguments above not explicitly 
+            listed in the signature, this is necessary for the function takes 
+            the key value pairs defined in the class
+
+        Return:
+        ----------
+        password_to_verify: str
+            If the password and verify_password match, returns the value in passowrd filed
+        
+        Raises:
+        ----------
+        ValueError
+            If the password and verify_passowrd does'nt match
+
         """
         if 'password' in values and password_to_verify != values['password']:
             raise ValueError('passwords do not match')

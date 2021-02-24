f1203e52d54ef97a4892d15312a458af69a489c4
Alejandro Campillo <alucardcampillo@gmail.com>
2021-02-24 10:07:18 -0500

    test folder created, applicant_user_test completed

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,33 @@
+import sys
+import pytest
+sys.path.append('./')
+from models import user
+
+# Test application_user class
+
+# Testing Emailstr field
+
+def test_valid_email_user():
+    assert user.applicant_user(email = "user@example.com") == {"email": "user@example.com"}
+
+# These tests for email should be fail always
+
+@pytest.mark.xfail(reason = "Invalid email")
+def test_invalid_email_without_dot():
+    assert user.applicant_user(email = "user@examplecom")
+
+@pytest.mark.xfail(reason = "Invalid email")
+def test_invalid_email_without_at_sign():
+    assert user.applicant_user(email = "userexample.com")
+
+@pytest.mark.xfail(reason = "Invalid email")
+def test_invalid_email_without_structure():
+    assert user.applicant_user(email = "user@.com")
+
+@pytest.mark.xfail(reason = "Invalid email")
+def test_invalid_email_invalid_type():
+    assert user.applicant_user(email = 121313254)
+
+@pytest.mark.xfail(reason = "Invalid email")
+def test_invalid_email_empty_field():
+    assert user.applicant_user(email = "")

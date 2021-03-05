b695b58d5b1e017e5154305082c8fee19b78e40a
Camilo-HG <22363097+Camilo-HG@users.noreply.github.com>
2021-03-05 15:19:20 -0500

    Modifying .env_example adding more parameters

--- settings.py
+++ settings.py
@@ -6,6 +6,10 @@ load_dotenv(find_dotenv())
 host = os.getenv('HOST')
 port = os.getenv('PORT')
 domain = os.getenv('DOMAIN')
+
+reload = os.getenv('RELOAD')
+access_log = os.getenv('ACCESS_LOG')
+
 applicant_path = os.getenv('APPLICANT_PATH')
 
-#print(domain)
+print(f'The app is running in domain: {domain}')

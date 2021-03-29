a3c61cf2e8a9450de9c890cba8755224285adef7
Santiago de Jesús Martínez Rodríguez <Smartinezr@intergrupo.com>
2021-03-29 12:17:09 -0500

    feat:implementation of configuration for connection to the frontend

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ import uvicorn
 from dotenv import dotenv_values
 from fastapi import FastAPI
 from routers import register_routers, auth_routers
-
-settings = dotenv_values(".env")
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.include_router(register_routers.router, tags=["Register"], prefix=settings['REGISTER_PATH'])
-app.include_router(auth_routers.router, tags=["Auth"], prefix=settings['AUTHENTICATION_PATH'])
+
+settings = dotenv_values(".env")
+origins = ["*"]
+
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["*"]
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(register_routers.router, tags=[
+                   "Register"], prefix=settings['REGISTER_PATH'])
+app.include_router(auth_routers.router, tags=[
+                   "Auth"], prefix=settings['AUTHENTICATION_PATH'])

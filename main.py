cd3c6330de1f8403f8d96e45323c4035eac5f6a8
Camilo-HG <22363097+Camilo-HG@users.noreply.github.com>
2021-03-30 11:50:35 -0500

    main.py and .env_example: refactoring code styles and removing unnecesary variables

--- main.py
+++ main.py
@@ -2,28 +2,37 @@ import uvicorn
 
 from dotenv import dotenv_values
 from fastapi import FastAPI
-from routers import register_routers, auth_routers
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+
+from routers import register_routers, auth_routers
+
 
 app = FastAPI()
 
 settings = dotenv_values(".env")
-origins = ["*"]
 
 app.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["*"]
-)
+    TrustedHostMiddleware,
+    allowed_hosts=settings["ALLOWED_HOSTS"]
+    )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    allow_origins=settings["ALLOWED_ORIGINS"],
+    allow_credentials=settings["ALLOW_CREDENTIALS"],
+    allow_methods=settings["ALLOWED_METHODS"],
+    allow_headers=settings["ALLOWED_HEADERS"]
+    )
 
-
-app.include_router(register_routers.router, tags=[
-                   "Register"], prefix=settings['REGISTER_PATH'])
-app.include_router(auth_routers.router, tags=[
-                   "Auth"], prefix=settings['AUTHENTICATION_PATH'])
+app.include_router(
+    register_routers.router,
+    tags=["Register"],
+    prefix=settings['REGISTER_PATH']
+    )
+    
+app.include_router(
+    auth_routers.router,
+    tags=["Auth"],
+    prefix=settings['AUTHENTICATION_PATH']
+    )

9707a3edeb489805a0d8a9e73ff00bedc42c3ba8
Santiago de Jesús Martínez Rodríguez <Smartinezr@intergrupo.com>
2021-04-08 12:37:31 -0500

    docs: adding driver documentation

--- routers/authentication.py
+++ routers/authentication.py
@@ -105,29 +105,146 @@ async def refresh_auth(user: user.auth_refresh):
 
 @router_of_authentication.post("/securityCodeRecoverylink")
 async def generate_security_code_link(user: user.user_email):
+    """
+         Generates a security code to reset the password, 
+         in addition to sending this code to the user's email address.
+
+         Parameters
+         ----------
+         - **email**: str
+            User email
+
+         Returns
+         ----------
+         - **response**: dict
+             email
+
+         Raises
+         ----------
+         - **HTTPException**:
+             If an error occurs at the time of user update
+         - **HTTPException**:
+             If the e-mail address doesn't match any of the users
+    """
 
     return send_security_code(user)
 
 
 @router_of_authentication.post("/validateSecuritycode")
 async def validate_security_code(user: user.security_code):
+    """
+         Check if the security code is correct
+
+         Parameters
+         ----------
+         - **email**: str
+            User email
+            
+         - **security_code**: str
+            Code entered by the user to be validated 
+
+         Returns
+         ----------
+         - **response**: dict
+             email
+
+         Raises
+         ----------
+         - **HTTPException**:
+             If the entered code doesn't match the generated code
+         - **HTTPException**:
+             If the e-mail address doesn't match any of the users
+    """
 
     return validate_securtity_code(user)
 
 
 @router_of_authentication.post("/passwordRecover")
 async def password_recover(user: user.recover_password):
+    """
+         Check if the security code is correct
+
+         Parameters
+         ----------
+         - **email**: str
+            User email
+            
+         - **new_password**: str
+            Code entered by the user to be validated 
+
+         - **new_verify_password**: str
+            verification password entered by the user to be validated 
+
+         Returns
+         ----------
+         - **response**: dict
+             passwordChanged 
+
+         Raises
+         ----------
+         - **HTTPException**:
+             If it was not possible to update your password
+         - **HTTPException**:
+             If the e-mail address doesn't match any of the users
+    """
 
     return update_password(user)
 
 
 @router_of_authentication.post("/qrRecoveryvinculation")
 async def qr_recovery_vinculation(user: user.user_email):
+    """
+         Returns the user's security questions
+
+         Parameters
+         ----------
+         - **email**: str
+            User email
+
+         Returns
+         ----------
+         - **response**: dict
+             securityQuestions
+             email 
+
+         Raises
+         ----------
+         - **HTTPException**:
+             If user has no security questions
+         - **HTTPException**:
+             If the e-mail address doesn't match any of the users
+    """
 
     return retrieve_security_questions(user)
 
 
 @router_of_authentication.post("/validateAnswers")
 async def validate_security_answers(user: user.enter_responses):
+    """
+         Validates the user's security questions
+         and returns the Qr link to the user.
+
+         Parameters
+         ----------
+         - **email**: str
+            User email
+         
+         - **answers**: list
+            Answers to security questions
+
+         Returns
+         ----------
+         - **response**: dict             
+             email 
+             urlPath
+             keyQr
+
+         Raises
+         ----------
+         - **HTTPException**:
+             If Invalid answers
+         - **HTTPException**:
+             If the e-mail address doesn't match any of the users
+    """
 
     return validate_security_questions(user)

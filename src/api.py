e544378867dc6e79faef39a8edf0f72c0098b7f7
David Peña <dpenah@intergrupo.com>
2021-04-27 11:53:27 -0500

    Update: Configurations and Typing

--- src/api.py
+++ src/api.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
+from src.config import settings
 from src.routers import authentication_routes, registry_routes
 
 app = FastAPI()
+
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=settings.get("ALLOWED_HOSTS", "*").split(",")
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=settings.get("ALLOWED_ORIGINS", "*").split(","),
+    allow_methods=settings.get("ALLOWED_METHODS", "*").split(","),
+    allow_headers=settings.get("ALLOWED_HEADERS", "*").split(",")
+)
 
 app.include_router(registry_routes)
 app.include_router(authentication_routes)
